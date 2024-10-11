71c1dc0d26e8cea50da41914428f4b909202f24a
jakub.koza <kubulax25@gmail.com>
2024-10-11 09:55:24 +0200

    Ukończono funkcjonalność back-end

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,5 +20,61 @@ namespace desktopowa
         {
             InitializeComponent();
         }
+
+        private void ChangePersonImage(object sender, RoutedEventArgs e)
+        {
+            int idNumber = int.Parse(personNumber.Text);
+
+            //source images/000-zdjecie.jpg
+            if (idNumber == 000)
+            {
+                personImage.Source = new BitmapImage(new Uri("img/000-zdjecie.jpg", UriKind.RelativeOrAbsolute));
+                fingerPrintImage.Source = new BitmapImage(new Uri("img/000-odcisk.jpg", UriKind.RelativeOrAbsolute));
+            }
+            else if (idNumber == 111)
+            {
+                personImage.Source = new BitmapImage(new Uri("img/111-zdjecie.jpg", UriKind.RelativeOrAbsolute));
+                fingerPrintImage.Source = new BitmapImage(new Uri("img/111-odcisk.jpg", UriKind.RelativeOrAbsolute));
+
+            }
+            else if (idNumber == 333)
+            {
+                personImage.Source = new BitmapImage(new Uri("img/333-zdjecie.jpg", UriKind.RelativeOrAbsolute));
+                fingerPrintImage.Source = new BitmapImage(new Uri("img/333-odcisk.jpg", UriKind.RelativeOrAbsolute));
+            }
+            else
+            {
+                personImage.Source = null;
+                fingerPrintImage.Source = null;
+            }
+        }
+
+        private void DisplayAddedData(object sender, RoutedEventArgs e)
+        {
+            string name = personName.Text;
+            string surname = personSurname.Text;
+            string eyeColor = "";
+
+            if (blue.IsChecked == true)
+            {
+                eyeColor = "niebieskie";
+            }
+            else if (green.IsChecked == true)
+            {
+                eyeColor = "zielone";
+            }
+            else if (peony.IsChecked == true)
+            {
+                eyeColor = "piwne";
+            }
+            else
+            {
+                eyeColor = "nie wybrano";
+            }
+
+            string message = name + " " + surname + " " + "kolor oczu " + eyeColor;
+
+            MessageBox.Show(message, "", MessageBoxButton.OK);
+        }
     }
 }
